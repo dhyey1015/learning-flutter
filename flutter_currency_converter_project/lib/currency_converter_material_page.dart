3775f2cbd8e4d6e246b287876e0aa3301509c18f
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-12-25 13:19:46 +0530

    currency app is complete

--- flutter_currency_converter_project/lib/currency_converter_material_page.dart
+++ flutter_currency_converter_project/lib/currency_converter_material_page.dart
@@ -1,20 +1,107 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget {
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
+  State<CurrencyConverterMaterialPage> createState() {
+    return _CurrencyConverterMaterialPageState();
+  }
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+  late double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 3,
+        style: BorderStyle.solid,
+        strokeAlign: BorderSide.strokeAlignCenter,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(color: Colors.white, fontSize: 35),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              'Hello World!!')
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'INR ${result != 0 ? result.toString() : result.toStringAsFixed(0)}',
+              style: const TextStyle(
+                color: Color.fromRGBO(255, 255, 255, 1),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: textEditingController,
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: "Please enter the Currency in USD",
+                  hintStyle: const TextStyle(color: Colors.black),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  //this "border" is defined globally inside the function
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(textEditingController.text) * 83;
+                  });
+                },
+                style: ButtonStyle(
+                  elevation: const WidgetStatePropertyAll(10),
+                  backgroundColor: const WidgetStatePropertyAll(
+                    Color.fromRGBO(0, 0, 0, 1),
+                  ),
+                  foregroundColor: const WidgetStatePropertyAll(
+                    Colors.white,
+                  ),
+                  minimumSize: const WidgetStatePropertyAll(
+                    Size(double.infinity, 50),
+                  ),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Convert',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
